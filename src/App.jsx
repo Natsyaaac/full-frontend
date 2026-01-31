c0c1e9fbc10ec56539cb702d87d39fbb3afa7a05
Natsyaaac <i81504799@gmail.com>
2026-01-31 15:48:49 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 // import GridOne from './components/one/Grid' 
-import Gridtwo from './components/two/Grid'
+// import Gridtwo from './components/two/Grid'
+// import GridThree from './components/three/Grid'
+import GridFour from './components/four/Grid'
 
 function App() {
 
   return (
     <>
-      <Gridtwo />
+      {/* <Gridtwo /> */}
       {/* <GridOne /> */}
+      {/* <GridThree /> */}
+      <GridFour />
     </>
   )
 }
